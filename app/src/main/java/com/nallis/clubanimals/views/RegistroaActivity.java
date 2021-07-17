9368ca5dac5ce58d586eec19706d8e801e762611
sebasjare08 <sebasjara@gmail.com>
2021-07-16 20:41:39 -0500

    inicio layout registro

--- app/src/main/java/com/nallis/clubanimals/views/RegistroaActivity.java
+++ app/src/main/java/com/nallis/clubanimals/views/RegistroaActivity.java
@@ -1,8 +1,10 @@
-package com.nallis.clubanimals;
+package com.nallis.clubanimals.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.nallis.clubanimals.R;
 
 public class RegistroaActivity extends AppCompatActivity {
 
