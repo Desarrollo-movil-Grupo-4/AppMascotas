f43793362903733e48689215101923ed652e3902
Nallis02 <75026477+Nallis02@users.noreply.github.com>
2021-07-18 18:38:37 -0500

    Login finalizado 2.0

--- app/src/main/java/com/nallis/clubanimals/views/MainActivity.java
+++ app/src/main/java/com/nallis/clubanimals/views/MainActivity.java
@@ -2,7 +2,9 @@ package com.nallis.clubanimals.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.nallis.clubanimals.R;
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToRegistro (View view){
+        Intent intent = new Intent(this, RegistroActivity);
+        startActivity(intent);
     }
 }
