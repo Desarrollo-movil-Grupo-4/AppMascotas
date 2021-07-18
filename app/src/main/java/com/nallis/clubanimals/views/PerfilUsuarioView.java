786916bd9595aa18eccf2f66770dbc8a53d6289b
sebasjare08 <sebasjara@gmail.com>
2021-07-18 15:53:44 -0500

    Redireccionamiento de botones

--- app/src/main/java/com/nallis/clubanimals/views/PerfilUsuarioView.java
+++ app/src/main/java/com/nallis/clubanimals/views/PerfilUsuarioView.java
@@ -2,7 +2,9 @@ package com.nallis.clubanimals.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.nallis.clubanimals.R;
 
@@ -12,5 +14,9 @@ public class PerfilUsuarioView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario_view);
+    }
+    public void goToIngresar(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
