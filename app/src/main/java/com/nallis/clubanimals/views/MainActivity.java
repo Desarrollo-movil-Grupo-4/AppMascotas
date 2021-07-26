ea4540405bcb48c5c762e242050343ecb0f0599c
Nallis02 <75026477+Nallis02@users.noreply.github.com>
2021-07-26 11:39:54 -0500

    clase con el profe mauricio 26 julio

--- app/src/main/java/com/nallis/clubanimals/views/MainActivity.java
+++ app/src/main/java/com/nallis/clubanimals/views/MainActivity.java
@@ -1,41 +1,68 @@
 package com.nallis.clubanimals.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nallis.clubanimals.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button boton;
+    private Button btn_logo;
+    private CheckBox sesion;
+    private EditText email;
+    private EditText contra;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_logo = findViewById(R.id.buttonLogin);
+        sesion = findViewById(R.id.sesion);
+        email = findViewById(R.id.Email);
+        contra = findViewById(R.id.Password);
+
+        preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+
+       /* if(revisarSesion()){
+            startActivity(new Intent(this, InicioActivityView.class));
+        }else{
+            Toast.makeText(getApplicationContext(), "debes iniciar sesion", Toast.LENGTH_SHORT).show();
+        }*/
+        email.setText(preferences.getString("email", " "));
+        contra.setText(preferences.getString("contra", " "));
+        btn_logo.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                //editor.putBoolean("sesion", sesion.isChecked());
+                editor.putString("email", email.getText().toString());
+                editor.putString("contra", contra.getText().toString());
+                editor.apply();
+
+                startActivity(new Intent(MainActivity.this, InicioActivityView.class));
+            }
+        });
+    }
+    private boolean revisarSesion(){
+        return this.preferences.getBoolean("sesion", false);
     }
 
-    public void goToVeterinaria (View view){
+    public void goToVeterinaria(View view) {
         Intent intent = new Intent(this, VeterinariaActivity.class);
-        startActivity(intent);
-    }
-/*
-    public void goToRegistro (View view){
-        Intent intent = new Intent(this, ContratarActivityView.class);
-
-    public void goToRegistro(View view){
-        Intent intent = new Intent(this, RegistroActivity.class);
-        startActivity(intent);
-    }
-    public void goToInicio(View view){
-        Intent intent = new Intent(this, InicioActivityView.class);
-        startActivity(intent);
-    }
-    public void goToContrasena(View view){
-        Intent intent = new Intent(this, RecuperarContrasenaView.class);
         startActivity(intent);
     }
 }
